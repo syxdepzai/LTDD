5a03acfe22d32e781931641a97a478744cada5c7
syxdepzai <theanh010104@gmail.com>
2025-11-21 14:06:23 +0700

    Clean BT5: remove IDE cache and prevent artifacts

--- BT5/app/src/main/java/com/example/bt5/RetrofitClient.java
+++ BT5/app/src/main/java/com/example/bt5/RetrofitClient.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://app.iotstar.vn:8081/appfoods/";
+    private static final String BASE_URL = "http://app.iotstar.vn:8081";
     private static Retrofit retrofit = null;
 
     public static Retrofit getRetrofit() {
